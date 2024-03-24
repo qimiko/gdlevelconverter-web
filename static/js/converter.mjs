18ef6ced174777a7e68b70a7718ca608910a9852
qimiko <25387744+qimiko@users.noreply.github.com>
2024-03-23 17:20:25 -0700

    cache bust

--- static/js/converter.mjs
+++ static/js/converter.mjs
@@ -2,7 +2,7 @@
 /**
  * indicates the path to where the conversion worker module can be found
  */
-const CONVERSION_WORKER_PATH = "./static/js/conversion_worker.mjs?v=4";
+const CONVERSION_WORKER_PATH = "./static/js/conversion_worker.mjs?v=5";
 
 /**
  * Represents a level
